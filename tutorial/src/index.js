d226ef5e539b6416a6101c4efba20d9e44909dd8
kodmm <tawaratsumida.kosei.infratop@gmail.com>
2020-04-09 16:25:48 +0900

    complete_Tutorial

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -25,7 +25,7 @@ function Square(props) {
       
       return (
         <div>
-          <div className="status">{status}</div>
+          
           <div className="board-row">
             {this.renderSquare(0)}
             {this.renderSquare(1)}
@@ -51,14 +51,15 @@ function Square(props) {
       super(props);
       this.state = {
         history: [{
-          suqares: Array(9).fill(null),
+          squares: Array(9).fill(null),
         }],
+        stepNumber: 0,
         xIsNext: true,
       };
     }
 
     handleClick(i) {
-      const history = this.state.history;
+      const history = this.state.history.slice(0, this.state.stepNumber + 1);
       const current = history[history.length - 1];
       const squares = current.squares.slice();
       if (calculateWinner(squares) || squares[i]) {
@@ -69,13 +70,21 @@ function Square(props) {
         history: history.concat([{
           squares: squares,
         }]),
+        stepNumber: history.length,
         xIsNext: !this.state.xIsNext,
       });
     }
 
+    jumpTo(step) {
+      this.setState({
+        stepNumber: step,
+        xIsnext: (step % 2) === 0,
+      })
+    }
+
     render() {
       const history = this.state.history;
-      const current = history[history.length - 1];
+      const current = history[this.state.stepNumber];
       const winner = calculateWinner(current.squares);
 
       const moves = history.map((step, move) => {
@@ -83,7 +92,7 @@ function Square(props) {
           'Go to move #' + move :
           'Go to game start';
         return(
-          <li>
+          <li key={move}>
             <button onClick={() => this.jumpTo(move)}>{desc}</button>
           </li>
         );
