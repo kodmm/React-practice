be6629209f16341acafca65275f247832166c1c7
kodmm <53547701+kodmm@users.noreply.github.com>
2020-12-04 16:34:43 +0900

    feature/Music search

--- hooks_practice/src/App.js
+++ hooks_practice/src/App.js
@@ -5,7 +5,7 @@ import Counter_hooks from './components/counter_hooks.js';
 import To_prevState from './components/to_prevState.js';
 import Effect_class from './components/effect_class.js';
 import Effect_hooks from './components/effect_hooks.js';
-
+import Music from './components/music.js'
 
 function App() {
   return (
@@ -18,6 +18,7 @@ function App() {
           <Route exact path="/To_prevState" component={To_prevState} />
           <Route exact path="/Effect_class" component={Effect_class} />
           <Route exact path="/Effect_hooks" component={Effect_hooks} />
+          <Route exact path="/Music" component={Music} />
         </Switch>
       </div>
     </BrowserRouter>
